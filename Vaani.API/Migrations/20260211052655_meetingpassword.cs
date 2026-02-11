089f7cb43cdf4341ee84a6ac35f1117382135eed
sanjaykrpandit <sanjuraj85@outlook.com>
2026-02-11 11:11:35 +0530

    meet pas

---
+++ Vaani.API/Migrations/20260211052655_meetingpassword.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vaani.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class meetingpassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "meetings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "meetings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequiresPassword",
+                table: "meetings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "meetings");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "meetings");
+
+            migrationBuilder.DropColumn(
+                name: "RequiresPassword",
+                table: "meetings");
+        }
+    }
+}
